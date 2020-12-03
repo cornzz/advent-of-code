b4d0a476d5296b14c15b844a25e8c5dd8e73e333
cornzz <39997278+cornzz@users.noreply.github.com>
2020-12-03 18:16:06 +0100

    Add 2, part two

--- 2/two.py
+++ 2/two.py
@@ -3,10 +3,13 @@ import numpy as np
 
 data = np.loadtxt('input.txt', str)
 
-num = 0
+num, num_new = 0, 0
 for line in data:
-    bounds = line[0].split('-')
-    if int(bounds[0]) <= line[2].count(line[1][0]) <= int(bounds[1]):
+    lower, upper = map(int, line[0].split('-'))
+    letter, password = line[1][0], line[2]
+    if lower <= password.count(letter) <= upper:
         num += 1
+    if (password[lower-1] == letter) != (password[upper-1] == letter):
+        num_new += 1
 
-print(num)
+print(f'{num}, new policy: {num_new}')
