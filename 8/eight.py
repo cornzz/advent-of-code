7c1288b3a9e272250c2d11f5fa914a3b6d17fc94
cornzz <39997278+cornzz@users.noreply.github.com>
2020-12-09 16:08:54 +0100

    Add 8

--- 8/eight.py
+++ 8/eight.py
@@ -1,14 +1,11 @@
-import numpy as np
-
-
-with open('input.txt') as f:
-    data = f.read().splitlines()
-    data = [x.split(' ') for x in data]
+def run(instructions):
     acc, i = 0, 0
     seen_inst = []
-    while i not in seen_inst:
+    while i < len(instructions):
+        if i in seen_inst:
+            return acc, True
         seen_inst.append(i)
-        inst, const = data[i]
+        inst, const = instructions[i]
         const = int(const)
         if inst == 'acc':
             acc += const
@@ -17,4 +14,19 @@ with open('input.txt') as f:
             continue
         i += 1
 
-    print(acc)
+    return acc, False
+
+
+with open('input.txt') as f:
+    data = f.read().splitlines()
+    data = [x.split(' ') for x in data]
+
+    to_change = [i for i in range(len(data)) if data[i][0] == 'jmp' or data[i][0] == 'nop']
+    for i in to_change:
+        d = data.copy()
+        d[i] = ['nop', d[i][1]] if d[i][0] == 'jmp' else ['jmp', d[i][1]]
+        accu, loop = run(d)
+        if not loop:
+            break
+
+    print(accu)
