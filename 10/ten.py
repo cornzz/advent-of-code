6fddc78cf50ce1b97a07c332e35141a396cd2758
cornzz <39997278+cornzz@users.noreply.github.com>
2020-12-11 10:44:16 +0100

    Improve 11, 10

--- 10/ten.py
+++ 10/ten.py
@@ -1,15 +1,15 @@
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)  # Cache previous calls
 def get_paths(i):
-    global paths
     if i == len(adapters) - 1:
         return 1
-    if i in paths:
-        return paths[i]
     num_paths = 0
     for j in range(i+1, min(i+4, len(adapters))):
         if adapters[j] - adapters[i] <= 3:
             num_paths += get_paths(j)
 
-    paths[i] = num_paths
     return num_paths
 
 
@@ -22,9 +22,6 @@ with open('input.txt') as f:
     diff = [adapters[i + 1] - adapters[i] for i in range(len(adapters) - 1)]
     p1 = diff.count(1) * diff.count(3)
 
-    paths = {}
     p2 = get_paths(0)
 
     print(f'Part one {p1}, Part two: {p2}')
-
-
--- 11/eleven.py
+++ 11/eleven.py
@@ -1,26 +1,20 @@
+from fileinput import input
 from copy import deepcopy
 from itertools import product
 
 
 def occ_adj_seats(r, c, d, p2=False):
     occ = 0
-    direction = [[x, y] for x, y in product(range(-1, 2), range(-1, 2)) if not (x == 0 and y == 0)]
-    for di in direction:
-        m = 1
-        while True:
-            try:
-                if r + di[0]*m < 0 or c + di[1]*m < 0:
-                    raise IndexError
-                cell = d[r + di[0]*m][c + di[1]*m]
-                if cell != '.':
-                    if cell == '#':
-                        occ += 1
-                    break
-                m += 1
-            except IndexError:
-                break
-            if not p2:
-                break
+    direction = [[x, y] for x, y in product([-1, 0, 1], [-1, 0, 1]) if not (x == 0 and y == 0)]
+    for dr, dc in direction:
+        rr = r + dr
+        cc = c + dc
+        if p2:
+            while 0 <= rr < R and 0 <= cc < C and d[rr][cc] == '.':
+                rr = rr + dr
+                cc = cc + dc
+        if 0 <= rr < R and 0 <= cc < C and d[rr][cc] == '#':
+            occ += 1
 
     return occ
 
@@ -29,21 +23,21 @@ def iterate(grid, p2=False):
     grid_tmp = []
     while grid != grid_tmp:
         grid_tmp = deepcopy(grid)
-        for row in range(len(grid)):
-            for col in range(len(grid[row])):
-                occ_adj = occ_adj_seats(row, col, grid_tmp, p2)
-                if grid[row][col] == 'L' and occ_adj == 0:
+        for row in range(R):
+            for col in range(C):
+                occ = occ_adj_seats(row, col, grid_tmp, p2)
+                if grid[row][col] == 'L' and occ == 0:
                     grid[row][col] = '#'
-                elif grid[row][col] == '#' and (not p2 and occ_adj >= 4 or occ_adj >= 5):
+                elif grid[row][col] == '#' and occ >= (4 if not p2 else 5):
                     grid[row][col] = 'L'
 
     return sum(row.count('#') for row in grid)
 
 
-with open('input.txt') as f:
-    data = f.read().splitlines()
-    data = list(map(list, data))
+data = [list(line.strip()) for line in input('input.txt')]
+R = len(data)
+C = len(data[0])
 
-    res_p1 = iterate(deepcopy(data))
-    res_p2 = iterate(deepcopy(data), p2=True)
-    print(f'Part one: {res_p1}, Part two: {res_p2}')
+res_p1 = iterate(deepcopy(data))
+res_p2 = iterate(deepcopy(data), p2=True)
+print(f'Part one: {res_p1}, Part two: {res_p2}')
