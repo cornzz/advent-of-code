950e22cf3692c946fe25f3455f1957d84d7e27cb
cornzz <39997278+cornzz@users.noreply.github.com>
2020-12-27 23:05:06 +0100

    Improve 17

--- 17/seventeen.py
+++ 17/seventeen.py
@@ -4,26 +4,27 @@ from itertools import product
 
 def boot(p2, d):
     active = set()
-    sz = len(d)
 
     for x, row in enumerate(d):
         for y, col in enumerate(row):
             if col == '#':
                 active.add((x, y, 0, 0))
 
+    offsets = [x for x in product([-1, 0, 1], repeat=4) if x != (0, 0, 0, 0)]
     for it in range(6):
-        new_active = set()
-        w_range = range(-1-it, 2+it) if p2 else [0]
-        for x in range(-1-it, sz+1+it):
-            for y in range(-1-it, sz+1+it):
-                for z in range(-1-it, 2+it):
-                    for w in w_range:
-                        num = 0
-                        for dx, dy, dz, dw in product([-1, 0, 1], repeat=4):
-                            if any([dx, dy, dz, dw]) and (x+dx, y+dy, z+dz, w+dw) in active:
-                                num += 1
-                        if ((x, y, z, w) in active and num in [2, 3]) or ((x, y, z, w) not in active and num == 3):
-                            new_active.add((x, y, z, w))
+        new_active, check = set(), set()
+        for x, y, z, w in active:
+            for dx, dy, dz, dw in offsets:
+                if p2 or w+dw == 0:
+                    check.add((x+dx, y+dy, z+dz, w+dw))
+
+        for x, y, z, w in check:
+            num = 0
+            for dx, dy, dz, dw in offsets:
+                if (x+dx, y+dy, z+dz, w+dw) in active:
+                    num += 1
+            if ((x, y, z, w) in active and num in [2, 3]) or ((x, y, z, w) not in active and num == 3):
+                new_active.add((x, y, z, w))
         active = new_active
 
     return len(active)
