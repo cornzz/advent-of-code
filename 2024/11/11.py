2b39582ff0951c5a0022d6056dd318a7f805e5a0
cornzz <39997278+cornzz@users.noreply.github.com>
2024-12-11 23:30:49 +0100

    Add 11

---
+++ 2024/11/11.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+
+X = [int(x) for x in open("input.txt").read().split()]
+
+@lru_cache(1000)
+def blink(num, d=0):
+	if d == 0:
+		return 1
+	numlen = len(str(num))
+	if num == 0:
+		return blink(1, d - 1)
+	elif numlen % 2 == 0:
+		left, right = int(str(num)[:numlen // 2]), int(str(num)[numlen // 2:])
+		return blink(left, d - 1) + blink(right, d - 1)
+	else:
+		return blink(num * 2024, d - 1)
+
+p1 = sum([blink(num, 25) for num in X])
+p2 = sum([blink(num, 75) for num in X])
+print(p1)
+print(p2)
+
+
